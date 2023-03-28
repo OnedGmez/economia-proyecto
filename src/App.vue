b7802915a951de2c05650486a5b56ba73cb0e795
OnedGmez <oned.gomez@unah.hn>
2023-03-27 21:12:54 -0600

    Cambios

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
+@font-face {
+    font-family: 'lobster';
+    src: url("assets/fonts/Lobster-Regular.ttf");
+}
+
+@font-face {
+    font-family: 'fredoka';
+    src: url("assets/fonts/Fredoka-VariableFont_wdth\,wght.ttf");
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
