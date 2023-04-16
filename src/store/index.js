a4e2ec271a38ed659b346f9295f85ab78426d48d
OnedGmez <oned.gomez@unah.hn>
2023-04-16 04:22:05 -0600

    Path de acceso a la carpeta con los svg de categorias y servicios en el S3

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,11 @@ import { defineStore } from 'pinia'
 import {ref} from 'vue'
 
 export const generalStore = defineStore('store', () => {
-
+  const pathCategorias = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/categories/'
+  const pathServicios = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/services/'
 
   return {
-
+    pathCategorias,
+    pathServicios
   }
 })
