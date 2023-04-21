1c76a91ef816a7ef7ec6aa94a37d3b5f317c2892
OnedGmez <oned.gomez@unah.hn>
2023-04-20 22:02:33 -0600

    Cambio al estilo del menú

--- src/components/complementos-componentes/menu-celular.vue
+++ src/components/complementos-componentes/menu-celular.vue
@@ -1,6 +1,6 @@
 <template>
-    <div :class="[scrolleando == true ? 'oculto' : '']" class="d-flex estatica-mobile-bottom">
-        <div @click="irPrincipal" class="seleccionado item-menu home-btn">
+    <div :class="[scrolleando == true ? 'oculto' : '', celular == false ? 'lg': '']" class="d-flex estatica-mobile-bottom">
+        <div :class="{seleccionado: opSeleccionada == '/'}" @click="irPrincipal" class="item-menu home-btn">
             <div class="icono-home">
                 <span class="icono"><font-awesome-icon icon="house" /> </span>
             </div>
@@ -8,7 +8,7 @@
                 <span>Home</span>
             </div>
         </div>
-        <div @click="" class="item-menu rentacar-btn">
+        <div :class="{seleccionado: opSeleccionada == '/rentacar'}" @click="navRentaCar" class="item-menu rentacar-btn">
             <div class="icono-rentAcar">
                 <span class="icono"><font-awesome-icon icon="car" /> </span>
             </div>
@@ -24,7 +24,7 @@
                 <span>¡Llevame!</span>
             </div>
         </div>
-        <div class="inicio-sesion item-menu">
+        <div  class="inicio-sesion item-menu">
             <div class="inicio-sesion-nombre">
                 <div @click="desplegarSubModal('Iniciar Sesion')" class="avatar">
                     <span class="icono"><font-awesome-icon icon="user-tie" /></span>
@@ -69,8 +69,11 @@
     z-index: 1;
     background-color: #fff;
     height: 60px;
-    right: 0;
-    left: 0;
+    left: 4.5%;
+    width: 90%;
+    box-shadow: 0px 1px 10px #8d8d8d52;
+    border-radius: 30px;
+    margin-bottom: 5px;
     transition: -ms-transform 0.25s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s, -webkit-transform 0.25s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s, transform 0.25s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s, visibility 0.25s ease 0s !important;
   }
 
@@ -83,7 +86,14 @@
 <script setup>
 import { ref } from 'vue';
 import router from '@/router';
+import { useRoute } from "vue-router";
 
+const routeParams = useRoute()
+const emisiones = defineEmits([
+  'abrirSesion'
+])
+
+const opSeleccionada = ref(routeParams.path)
 const scrolleando = ref(false)
 const lastScroll = ref(0);
 /**
@@ -91,9 +101,12 @@ const lastScroll = ref(0);
  * Función que está bindeada al botón home en la vista de celular, redireccionará a la página principal o hará la recarga de la página
  */
  const irPrincipal = () => {
-  router.go()
+  router.push('/')
 }
 
+const navRentaCar = () =>{
+  router.push('/rentacar')
+}
 
 //Evento que sirve para escuchar el scroll de la página
 window.addEventListener("scroll", () => {
@@ -112,4 +125,9 @@ window.addEventListener("scroll", () => {
   }
   lastScroll.value = currentScroll;
 });
+
+
+const desplegarSubModal = (accion) =>{
+  emisiones('abrirSesion', accion)
+}
 </script>
