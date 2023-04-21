7b8d50474accb8adb2ed89cc36d8a93761997c5d
OnedGmez <oned.gomez@unah.hn>
2023-04-20 19:33:07 -0600

    Menu celular convertido en complemento reutilizable y cambio de botón de filtros en vista de móvil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,20 @@
     <div class="pagina">
       <div class="estatica-top">
         <div class="cabecera">
-          <div class="logo">
+          <div v-if="mostrarMenuCel == false" class="logo">
             <div class="img">
               <img class="img-fluid" src="../assets/img/logo.png" alt="Logo RentAway">
             </div>
           </div>
           <div class="busqueda">
             <barraBusqueda />
+            <div v-if="mostrarMenuCel == true" @click="desplegarOtrosFiltros" class="filtro-btn">
+              <div class="icono-boton">
+                <span class="icono"> <font-awesome-icon icon="sliders" /> </span>
+              </div>
+            </div>
           </div>
-          <div class="inicio-sesion">
+          <div v-if="mostrarMenuCel == false" class="inicio-sesion">
             <div class="d-flex inicio-sesion-nombre">
               <div class="nombre-usuario">
                 <span>Invitado</span>
@@ -24,7 +29,7 @@
         </div>
         <div class="d-flex zona-filtros">
           <zonaFiltros />
-          <button @click="desplegarOtrosFiltros" class="d-flex btn">
+          <button v-if="mostrarMenuCel == false" @click="desplegarOtrosFiltros" class="d-flex btn">
             <div class="icono-boton">
               <span> <font-awesome-icon icon="sliders" /> </span>
             </div>
@@ -33,6 +38,7 @@
             </div>
           </button>
         </div>
+
       </div>
       <div class="main container">
         <div class="row">
@@ -40,41 +46,14 @@
             @abrir-detalles="(detalle) => abrirModalDetalles(detalle)" />
         </div>
       </div>
-      <div v-if="mostrarMenuCel == true" :class="[scrolleando == true ? 'oculto' : '']"
-        class="d-flex estatica-mobile-bottom">
-        <div @click="irPrincipal" class="item-menu home-btn">
-          <div class="icono-home">
-            <span class="icono"><font-awesome-icon icon="house" /> </span>
-          </div>
-          <div class="text-btn">
-            <span>Home</span>
-          </div>
-        </div>
-        <div @click="desplegarOtrosFiltros" class="item-menu filtro-btn">
-          <div class="icono-boton">
-            <span class="icono"> <font-awesome-icon icon="sliders" /> </span>
-          </div>
-          <div class="text-btn">
-            <span>Filtros</span>
-          </div>
-        </div>
-        <div class="inicio-sesion item-menu">
-          <div class="inicio-sesion-nombre">
-            <div @click="desplegarSubModal('Iniciar Sesion')" class="avatar">
-              <span class="icono"><font-awesome-icon icon="user-tie" /></span>
-            </div>
-            <div class="text-btn">
-              <span>Invitado</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <menuCelular v-if="mostrarMenuCel == true"/>
     </div>
     <accionesSesion v-if="mostrandoModalAccionesSesion == true"
       @desplegar-modal="(nombreModal) => { desplegarSubModal(nombreModal), desplegarModalSesion() }" />
     <modalSesion v-if="mostrandoModalSesion == true" :accion="accionModal" @cerrar-modal="() => cerrarSubModal()" />
     <modalOtrosFiltros v-if="mostrandoModalFiltros == true" @cerrar-modal="() => desplegarOtrosFiltros()" />
-    <modalDetallesCasa v-if="mostrarDetallesCasa == true" :data="informacionDetalleCasa" @cerrar-detalles="() => cerrarModalDetalles()"/>
+    <modalDetallesCasa v-if="mostrarDetallesCasa == true" :data="informacionDetalleCasa"
+      @cerrar-detalles="() => cerrarModalDetalles()" />
   </div>
 </template>
 
@@ -88,18 +67,17 @@ import tarjetaCasas from '@/components/tarjeta-casa.vue'
 import zonaFiltros from '@/components/carrusel-filtro.vue';
 import modalOtrosFiltros from '@/components/modal-otros-filtros.vue'
 import modalDetallesCasa from '@/components/modal-detalles-casa.vue';
-import router from '@/router';
+import menuCelular from '@/components/complementos-componentes/menu-celular.vue';
 
 const accionModal = ref('')
 
 const mostrandoModalAccionesSesion = ref(false)
 const mostrandoModalSesion = ref(false)
 const mostrandoModalFiltros = ref(false)
-const scrolleando = ref(false)
+
 const mostrarMenuCel = ref(false)
 const mostrarDetallesCasa = ref(false)
 const informacionDetalleCasa = ref([{}])
-const lastScroll = ref(0);
 
 
 const dataPrueba = [
@@ -172,35 +150,9 @@ const abrirModalDetalles = (data) => {
   informacionDetalleCasa.value = data
 }
 
-const cerrarModalDetalles = () =>{
+const cerrarModalDetalles = () => {
   mostrarDetallesCasa.value = false
 }
-
-/**
- * dev: Oned Gómez 
- * Función que está bindeada al botón home en la vista de celular, redireccionará a la página principal o hará la recarga de la página
- */
-const irPrincipal = () => {
-  router.go()
-}
-
-//Evento que sirve para escuchar el scroll de la página
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll == 0) {
-    scrolleando.value = false
-    return;
-  }
-
-  if (currentScroll > lastScroll.value) {
-    // down 
-    scrolleando.value = true
-  } else if (currentScroll < lastScroll.value) {
-    // up 
-    scrolleando.value = false
-  }
-  lastScroll.value = currentScroll;
-});
 
 if (screen.width <= 768) {
   mostrarMenuCel.value = true
@@ -313,33 +265,6 @@ if (screen.width <= 768) {
 }
 
 /*
-  * CSS barra inferior para dispositivos celulares
- */
-.pagina .estatica-mobile-bottom {
-  display: none;
-  justify-content: space-evenly;
-  padding: 10px 0;
-  color: #000000;
-}
-
-.pagina .estatica-mobile-bottom .item-menu {
-  color: #8e8e8e;
-}
-
-.pagina .estatica-mobile-bottom .item-menu:hover {
-  color: #ff475e;
-}
-
-.pagina .estatica-mobile-bottom .icono {
-  font-size: calc(1em + .75vw);
-}
-
-.pagina .estatica-mobile-bottom .text-btn {
-  font-weight: 600;
-  font-size: calc(.8em + .45vw);
-}
-
-/*
   * Configuramos el media query para dispositivo celulares 
  */
 @media (max-width: 768px) {
@@ -347,12 +272,6 @@ if (screen.width <= 768) {
     padding: 0 !important;
     box-shadow: 0px 3px 5px #82828252;
     margin-bottom: 15px;
-  }
-
-  .pagina .estatica-top .cabecera .logo,
-  .pagina .estatica-top .cabecera .inicio-sesion,
-  .pagina .estatica-top .zona-filtros .btn {
-    display: none !important;
   }
 
   .pagina .estatica-top .cabecera {
@@ -363,23 +282,16 @@ if (screen.width <= 768) {
 
   .pagina .estatica-top .cabecera .busqueda {
     margin: 0;
-  }
-
-  .pagina .estatica-mobile-bottom {
     display: flex;
-    position: fixed;
-    bottom: 0;
-    z-index: 1;
-    background-color: #fff;
-    height: 65px;
-    right: 0;
-    left: 0;
-    transition: -ms-transform 0.25s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s, -webkit-transform 0.25s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s, transform 0.25s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s, visibility 0.25s ease 0s !important;
   }
 
-  .pagina .oculto {
-    transform: translateY(100%);
-    visibility: hidden;
+  .pagina .estatica-top .cabecera .busqueda .filtro-btn {
+    margin: auto 0 auto 15px;
+  }
+
+  .pagina .estatica-top .cabecera .busqueda .filtro-btn .icono-boton {
+    font-size: calc(1em + .65vw) !important;
+    color: #6907f2 !important;
   }
 }
 </style>
