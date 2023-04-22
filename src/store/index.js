b612ff587ec702e4a7ead2ec5a9d7af3329be1a9
OnedGmez <oned.gomez@unah.hn>
2023-04-21 19:03:50 -0600

    Cambio en el path de acceso al servidor de archivos

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@ import { defineStore } from 'pinia'
 import {ref} from 'vue'
 
 export const generalStore = defineStore('store', () => {
-  const pathCategorias = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/categories/'
-  const pathServicios = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/services/'
+  const pathCategorias = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/iconos/categories/'
+  const pathServicios = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/iconos/services/'
 
   return {
     pathCategorias,
--- src/store/rent-a-car.js
+++ src/store/rent-a-car.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import {ref} from 'vue'
 
 export const rentCarStore = defineStore('rentCar', () => {
-  const pathVehiculos = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/cars/'
+  const pathVehiculos = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/photos/cars/'
   
   return {
     pathCategorias,
