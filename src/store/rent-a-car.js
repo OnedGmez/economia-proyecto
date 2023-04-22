752d6a3f2749052678048a6cab374245e61ebd26
OnedGmez <oned.gomez@unah.hn>
2023-04-22 01:57:01 -0600

    Limpiando de elementos inutiles

--- src/store/rent-a-car.js
+++ src/store/rent-a-car.js
@@ -2,10 +2,8 @@ import { defineStore } from 'pinia'
 import {ref} from 'vue'
 
 export const rentCarStore = defineStore('rentCar', () => {
-  const pathVehiculos = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/photos/cars/'
   
   return {
-    pathCategorias,
-    pathServicios
+
   }
 })
