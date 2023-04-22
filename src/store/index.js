5d89cfcfd9240f629017d105e9d3e1edff8c11ad
OnedGmez <oned.gomez@unah.hn>
2023-04-22 01:56:23 -0600

    Función para guardar los archivos en el bucket de Supabase

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,39 @@
 import { defineStore } from 'pinia'
-import {ref} from 'vue'
+import { ref } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
 
 export const generalStore = defineStore('store', () => {
-  const pathCategorias = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/iconos/categories/'
-  const pathServicios = 'https://rentaway-file-serve.s3.amazonaws.com/rentaway/iconos/services/'
+
+
+  const subirArchivo = async (nombreA, carpeta, file) => {
+    const rutaParcial = ref('')
+    try {
+      const ruta = carpeta + '/' + nombreA
+      const { data, error } = await supabase
+        .storage
+        .from('digital-economy-file-server')
+        .upload(ruta, file, {
+          cacheControl: '3600',
+          upsert: false
+        })
+
+        if(data !== ''){
+          rutaParcial.value = data
+        }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      const { data } = supabase
+        .storage
+        .from('digital-economy-file-server')
+        .getPublicUrl(rutaParcial.value)
+      
+      return data
+    }
+  }
+
 
   return {
-    pathCategorias,
-    pathServicios
+    subirArchivo
   }
 })
